e96fb98c7628419ba56ba575bd9108657de605a6
Ben <allredbm@yahoo.com>
2018-12-05 16:56:20 -0500

    Initial commit

---
+++ src/main/java/com/techtalentsouth/MyGallery/MyGalleryApplication.java
@@ -0,0 +1,12 @@
+package com.techtalentsouth.MyGallery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyGalleryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyGalleryApplication.class, args);
+	}
+}
